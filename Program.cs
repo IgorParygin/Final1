b466682a4800e4e31873acf127ba0b3c2e5cd605
Igor <igor_parygin@mail.ru>
2022-11-12 19:53:01 +0300

    Создал метод отбора элементов массива меньше 3 символов

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿string[] array = new string[5] { "hello", "2", "world", ":-)"};
+﻿string[] array = new string[5] {"hello", "2", "world", ":-)"};
 string[] mass = new string[array.Length];
+
+void Arrayless3(string[] array, string[] mass)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        mass[count] = array[i];
+        count++;
+        }
+    }
+}
